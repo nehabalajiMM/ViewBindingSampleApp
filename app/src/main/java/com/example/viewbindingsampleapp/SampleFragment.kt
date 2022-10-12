668e3cb0832a3ab69e4920cc8c6864d940ef8e32
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-12 12:23:53 +0530

    setup viewbinding in fragment

--- app/src/main/java/com/example/viewbindingsampleapp/SampleFragment.kt
+++ app/src/main/java/com/example/viewbindingsampleapp/SampleFragment.kt
@@ -7,11 +7,15 @@ import com.example.viewbindingsampleapp.databinding.FragmentSampleBinding
 
 class SampleFragment : Fragment(R.layout.fragment_sample) {
 
-    private lateinit var binding: FragmentSampleBinding
+    private var binding: FragmentSampleBinding ? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSampleBinding.bind(view)
-        binding.tvHelloWorldFragment
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
     }
 }
